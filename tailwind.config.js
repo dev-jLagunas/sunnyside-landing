5c7c56475e7c8dc01ca55b58dfd9abd713fd104e
Juan Lagunas <dev.jlagunas@gmail.com>
2024-01-19 14:27:53 +0900

    Create HTML skeleton structure in preparation for dynamic population and styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,18 +3,6 @@ export default {
   purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      colors: {
-        "soft-red": "hsl(7, 99%, 70%)",
-        yellow: "hsl(51, 100%, 49%)",
-        "dark-cyan": "hsl(167, 40%, 24%)",
-        "dark-blue": "hsl(198, 62%, 26%)",
-        "footer-cyan": "hsl(168, 34%, 41%)",
-        "neutral-dark-blue": "hsl(212, 27%, 19%)",
-        "neutral-dark-gray": "hsl(213, 9%, 39%)",
-        "neutral-gray": "hsl(232, 10%, 55%)",
-        "neutral-light-gray": "hsl(210, 4%, 67%)",
-        white: "hsl(0, 0%, 100%)",
-      },
       fontFamily: {
         barlow: ["Barlow", "sans-serif"],
         fraunces: ["Fraunces", "serif"],
