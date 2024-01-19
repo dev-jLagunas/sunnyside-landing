3774e6142563a6fcfd2bacad4f4872fe365bdf50
Juan Lagunas <dev.jlagunas@gmail.com>
2024-01-19 12:00:32 +0900

    Create navbar html content and toggle open/close functionality

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,12 @@
 import "./style.scss";
+
+let toggleBtn = document.getElementById("navbar-toggle")!;
+let menu = document.getElementById("navbar-menu")!;
+
+toggleBtn.addEventListener("click", function () {
+  if (menu.style.display === "none") {
+    menu.style.display = "block";
+  } else {
+    menu.style.display = "none";
+  }
+});
